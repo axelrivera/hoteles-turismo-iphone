92cd12ca876b51a72b99b1d70f4c3025e4ea3762
Axel Rivera <axel@riveralabs.com>
2013-06-06 08:47:17 -0400

    Initial Commit

---
+++ TurismoPR/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TurismoPR
+//
+//  Created by Axel Rivera on 6/6/13.
+//  Copyright (c) 2013 Axel Rivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
